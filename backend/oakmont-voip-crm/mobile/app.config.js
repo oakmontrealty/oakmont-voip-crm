bf48341bf4904f9650218769bc5bc545fe254293
oakmontrealty <terence@oakmontrealty.com.au>
2025-08-05 14:26:42 -0700

    Update app.config.js

--- backend/oakmont-voip-crm/mobile/app.config.js
+++ backend/oakmont-voip-crm/mobile/app.config.js
@@ -2,6 +2,9 @@ export default {
   expo: {
     name: 'Oakmont Voip CRM',
     slug: 'oakmontcrmvoip',
+    ios: {
+      bundleIdentifier: 'com.oakmont.voipcrm',
+    },
     extra: {
       eas: {
         projectId: '79c0991a-221c-4171-860c-8b8b89bf8f76'
