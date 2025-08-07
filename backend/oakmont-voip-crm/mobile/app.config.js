aa3e6f1f6a89f0133eb8984d5aa1aeb7e321b0d6
oakmontrealty <terence@oakmontrealty.com.au>
2025-08-06 20:51:53 -0700

    Add Android package to app.config.js

--- backend/oakmont-voip-crm/mobile/app.config.js
+++ backend/oakmont-voip-crm/mobile/app.config.js
@@ -5,6 +5,10 @@ export default {
     ios: {
       bundleIdentifier: 'com.oakmont.voipcrm',
     },
+        android: {
+      package: 'com.oakmont.voipcrm',
+    },
+
     extra: {
       eas: {
         projectId: '79c0991a-221c-4171-860c-8b8b89bf8f76'
